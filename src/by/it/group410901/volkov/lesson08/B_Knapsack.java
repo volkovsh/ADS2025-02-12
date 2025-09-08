100ef07832da8dd00080c8aa88e9e6a5680100d9
volkov_sh <shurikvolkov08@gmail.com>
2025-09-08 13:00:12 +0300

    commit

--- src/by/it/group410901/volkov/lesson08/B_Knapsack.java
+++ src/by/it/group410901/volkov/lesson08/B_Knapsack.java
@@ -4,51 +4,37 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
-/*
-Задача на программирование: рюкзак без повторов
-
-Первая строка входа содержит целые числа
-    1<=W<=100000     вместимость рюкзака
-    1<=n<=300        число золотых слитков
-                    (каждый можно использовать только один раз).
-Следующая строка содержит n целых чисел, задающих веса каждого из слитков:
-  0<=w[1]<=100000 ,..., 0<=w[n]<=100000
-
-Найдите методами динамического программирования
-максимальный вес золота, который можно унести в рюкзаке.
-
-Sample Input:
-10 3
-1 4 8
-Sample Output:
-9
-
-*/
-
 public class B_Knapsack {
 
-    int getMaxWeight(InputStream stream ) {
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+    int getMaxWeight(InputStream stream) {
         Scanner scanner = new Scanner(stream);
-        int w=scanner.nextInt();
-        int n=scanner.nextInt();
-        int gold[]=new int[n];
+        int W = scanner.nextInt();  // вместимость рюкзака
+        int n = scanner.nextInt();  // количество слитков
+        int[] gold = new int[n];    // веса слитков
+
         for (int i = 0; i < n; i++) {
-            gold[i]=scanner.nextInt();
+            gold[i] = scanner.nextInt();
         }
 
+        // dp[w] - максимальный вес для рюкзака вместимостью w
+        int[] dp = new int[W + 1];
 
-        int result = 0;
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        for (int i = 0; i < n; i++) {
+            // Идем справа налево чтобы избежать повторного использования слитков
+            for (int w = W; w >= gold[i]; w--) {
+                if (dp[w - gold[i]] + gold[i] > dp[w]) {
+                    dp[w] = dp[w - gold[i]] + gold[i];
+                }
+            }
+        }
+
+        return dp[W];
     }
-
 
     public static void main(String[] args) throws FileNotFoundException {
         InputStream stream = B_Knapsack.class.getResourceAsStream("dataB.txt");
         B_Knapsack instance = new B_Knapsack();
-        int res=instance.getMaxWeight(stream);
+        int res = instance.getMaxWeight(stream);
         System.out.println(res);
     }
-
 }
