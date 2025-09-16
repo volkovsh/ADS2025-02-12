6da2482860dbd7129ab18e968ad00049b1027d1f
volkov_sh <shurikvolkov08@gmail.com>
2025-09-16 13:13:26 +0300

    commit

--- src/by/it/group410901/volkov/lesson08/A_Knapsack.java
+++ src/by/it/group410901/volkov/lesson08/A_Knapsack.java
@@ -7,9 +7,39 @@ import java.util.BitSet;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
+/*
+Задача на программирование: рюкзак с повторами
+
+Первая строка входа содержит целые числа
+    1<=W<=100000     вместимость рюкзака
+    1<=n<=300        сколько есть вариантов золотых слитков
+                     (каждый можно использовать множество раз).
+Следующая строка содержит n целых чисел, задающих веса слитков:
+  0<=w[1]<=100000 ,..., 0<=w[n]<=100000
+
+Найдите методами динамического программирования
+максимальный вес золота, который можно унести в рюкзаке.
+
+
+Sample Input:
+10 3
+1 4 8
+Sample Output:
+10
+
+Sample Input 2:
+
+15 3
+2 8 16
+Sample Output 2:
+14
+
+*/
+
 public class A_Knapsack {
 
     int getMaxWeight(InputStream stream) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         Scanner scanner = new Scanner(stream);
         int W = scanner.nextInt();
         int n = scanner.nextInt();
@@ -26,6 +56,8 @@ public class A_Knapsack {
                 .forEach(w -> Arrays.stream(gold)
                         .filter(weight -> w + weight <= W)
                         .forEach(weight -> achievable.set(w + weight)));
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
         // Находим максимальный достижимый вес
         return achievable.previousSetBit(W);
--- src/by/it/group410901/volkov/lesson08/C_Stairs.java
+++ src/by/it/group410901/volkov/lesson08/C_Stairs.java
@@ -4,6 +4,33 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
+/*
+Даны число 1<=n<=100 ступенек лестницы и
+целые числа −10000<=a[1],…,a[n]<=10000, которыми помечены ступеньки.
+Найдите максимальную сумму, которую можно получить, идя по лестнице
+снизу вверх (от нулевой до n-й ступеньки), каждый раз поднимаясь на
+одну или на две ступеньки.
+
+Sample Input 1:
+2
+1 2
+Sample Output 1:
+3
+
+Sample Input 2:
+2
+2 -1
+Sample Output 2:
+1
+
+Sample Input 3:
+3
+-1 2 1
+Sample Output 3:
+3
+
+*/
+
 public class C_Stairs {
 
     int getMaxSum(InputStream stream) {
